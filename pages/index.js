851f6a6e1bb9ac08c1f6c968ac1e25d5c62b3ae0
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-12-14 21:29:37 -0300

    Portfolio Starter

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>FDA Portfolio</title>
+        <meta name="description" content="My portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Mi Portfolio</h1>
+    </div>
+  );
+}
