f9542255ad837652bfc71f5c4c622b161578eecd
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-08 01:25:49 -0300

    ✨ MUI theme for the font

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 
 import Layout from '../components/shared/Layout';
 
-import { Box } from '@mui/material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 
 import createEmotionCache from '../config/createEmotionCache';
 import { CacheProvider } from '@emotion/react';
@@ -11,18 +11,26 @@ import { AnimatePresence } from 'framer-motion';
 
 const clientSideEmotionCache = createEmotionCache();
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'NokiaFC22',
+  },
+});
+
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
-      <AnimatePresence>
-        {/* bg-[url('/pxbg.jpg')] */}
-        <Box className="bg-[url('/pxbg2.png')] bg-cover h-[100vh] w-full">
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Box>
-      </AnimatePresence>
+      <ThemeProvider theme={theme}>
+        <AnimatePresence>
+          {/* bg-[url('/pxbg.jpg')] */}
+          <Box className="bg-[url('/pxbg2.png')] bg-cover h-[100vh] w-full">
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Box>
+        </AnimatePresence>
+      </ThemeProvider>
     </CacheProvider>
   );
 }
