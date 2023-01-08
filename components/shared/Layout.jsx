f86f025d2838eb4b2a9f6f0d8c70b45caee0df84
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-08 01:26:41 -0300

    ✨ Layout styling

--- components/shared/Layout.jsx
+++ components/shared/Layout.jsx
@@ -2,7 +2,7 @@ import { Container } from '@mui/material';
 
 export default function Layout({ children }) {
   return (
-    <Container maxWidth="sm" className="bg-[#2c2c2c] bg-opacity-50 h-full ">
+    <Container maxWidth="sm" className="bg-[#2c2c2c] px-[54px] py-[24px] bg-opacity-50 h-full ">
       {children}
     </Container>
   );
