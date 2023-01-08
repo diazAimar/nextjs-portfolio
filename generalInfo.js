35d741414c454bc10617ac9982fba3f46d2e51a1
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-08 01:26:22 -0300

    ✨ js file with data arrays

---
+++ generalInfo.js
@@ -0,0 +1,53 @@
+export const skills = [
+  { id: 1, name: 'HTML - CSS' },
+  { id: 2, name: 'BOOTSTRAP - TAILWIND CSS' },
+  { id: 3, name: 'JAVASCRIPT' },
+  { id: 4, name: 'PHP - LARAVEL' },
+  { id: 5, name: 'REACT JS - NEXT JS' },
+];
+
+export const education = [
+  {
+    id: 1,
+    institution: 'Universidad Nacional del Comahue',
+    name: 'August 2020 - December 2022 | Associate Degree in Web Development (finished)',
+  },
+  {
+    id: 2,
+    institution: 'Centro Pyme Adeneu - Plan 10mil - PAE - Fundación Pescar y EducacionIT',
+    name: 'July 2021 - December 2021 | Full Stack Programming Course and Soft Skills Course',
+  },
+  {
+    id: 3,
+    institution: 'Universidad Nacional del Comahue',
+    name: 'January 2020 - August 2020 | Computer Science Bachelors Degree (2 subjects approved)',
+  },
+  {
+    id: 5,
+    institution: 'Universidad Nacional de Córdoba',
+    name: '2016 - 2019 | Computer Science Bachelors Degree (6 subjects approved)',
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    name: 'growgrove',
+    college: 1,
+    personal: 1,
+    shortDescription:
+      'web application focused on improving the users productivity (inspired by Forest)',
+  },
+  {
+    id: 2,
+    name: 'juntar',
+    college: 1,
+    personal: 0,
+    shortDescription: 'web application created to manage events',
+  },
+  {
+    id: 3,
+    name: 'juntar',
+    shortDescription: 'web application created to manage events related to school (college)',
+  },
+];
