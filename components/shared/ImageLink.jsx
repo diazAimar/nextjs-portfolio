9514b9e40a639bdc1fc4a44736f6f529265aa6f8
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-15 01:28:33 -0300

    ✨ Changed styles and new menu code

--- components/shared/ImageLink.jsx
+++ components/shared/ImageLink.jsx
@@ -1,17 +1,56 @@
-import { Typography } from '@mui/material';
-
 import Link from 'next/link';
 
 import Tooltip from 'rc-tooltip';
 import 'rc-tooltip/assets/bootstrap_white.css';
 import ConditionalTooltipWrapper from '../home/ConditionalTooltipWrapper';
 
+import { Box, Typography } from '@mui/material';
+
 export default function ImageLink({ href, target, ext, sqr }) {
   return (
-    <Link
+    <Box
+      className={
+        `shadow-[0_10px_0_0_rgba(14,7,27,1)] active:shadow-none active:translate-y-[10px] rounded-[15px] relative` +
+        (sqr ? ` w-[88px] sm:w-[110px] ` : ` w-[200px] sm:w-[250px]`)
+      }
+    >
+      <ConditionalTooltipWrapper
+        target={target}
+        wrapper={(children) => (
+          <Tooltip
+            placement="bottom"
+            trigger={['hover']}
+            overlay={<Typography className="font-NokiaFC22">{target}</Typography>}
+          >
+            {children}
+          </Tooltip>
+        )}
+      >
+        <Link href={href} target={ext ? '_blank' : '_self'} className="">
+          <video
+            src={`/assets/${target}-button/${target}-button-hover.webm`}
+            className=""
+            onMouseOver={(e) => {
+              e.currentTarget.play();
+              console.log('as');
+            }}
+            onMouseOut={(e) => {
+              e.currentTarget.pause();
+              e.currentTarget.currentTime = 0;
+            }}
+            muted
+            loop
+          >
+            Your browser does not support the <code>video</code> element.
+          </video>
+        </Link>
+      </ConditionalTooltipWrapper>
+    </Box>
+    /*    <Link
       href={href}
       className={
-        'h-80px sm:h-[100px]' + (sqr ? ` w-[88px] sm:w-[110px] ` : ` w-[200px] sm:w-[250px]`)
+        ` relative h-80px sm:h-[100px]` +
+        (sqr ? ` w-[88px] sm:w-[110px] ` : ` w-[200px] sm:w-[250px]`)
       }
       target={ext ? '_blank' : '_self'}
     >
@@ -27,25 +66,25 @@ export default function ImageLink({ href, target, ext, sqr }) {
           </Tooltip>
         )}
       >
-        <picture className="h-full flex items-end">
-          <img
-            src={`/assets/${target}-button/${target}-button.png`}
-            onMouseOver={(e) =>
-              (e.currentTarget.src = `/assets/${target}-button/${target}-button-hover.gif`)
-            }
-            onMouseOut={(e) =>
-              (e.currentTarget.src = `/assets/${target}-button/${target}-button.png`)
-            }
-            onMouseUp={(e) =>
-              (e.currentTarget.src = `/assets/${target}-button/${target}-button.png`)
-            }
-            onMouseDown={(e) =>
-              (e.currentTarget.src = `/assets/${target}-button/${target}-button-clicked.png`)
-            }
-            alt={target + ' button'}
-          />
-        </picture>
+        <video
+          src={`/assets/${target}-button/${target}-button-hover.webm`}
+          className={clickedButtonStyles + ` flex items-end rounded-[15px]`}
+          onMouseOver={(e) => e.currentTarget.play()}
+          onMouseOut={(e) => {
+            e.currentTarget.pause();
+            e.currentTarget.currentTime = 0;
+            // handleMouseUp();
+            // e.currentTarget.src = `/assets/${target}-button/${target}-button-hover.mp4`;
+          }}
+          // onMouseUp={() => handleMouseUp()}
+          // onMouseDown={() => handleMouseClick()}
+          poster=""
+          muted
+          loop
+        >
+          Tu navegador no admite el elemento <code>video</code>.
+        </video>
       </ConditionalTooltipWrapper>
-    </Link>
+    </Link> */
   );
 }
