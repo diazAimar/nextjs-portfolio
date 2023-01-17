c951f71eead4396e1206570dc064254e28c54d6c
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-16 22:06:59 -0300

    ✨ Material ui theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '../styles/globals.css';
+import { defaultTheme } from '../styles/defaultTheme';
 
 import Layout from '../components/shared/Layout';
 
-import { Box, createTheme, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 
 import createEmotionCache from '../config/createEmotionCache';
 import { CacheProvider } from '@emotion/react';
@@ -11,17 +12,11 @@ import { AnimatePresence } from 'framer-motion';
 
 const clientSideEmotionCache = createEmotionCache();
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Abadon',
-  },
-});
-
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={defaultTheme}>
         <AnimatePresence>
           {/* bg-[url('/pxbg.jpg')] */}
           <Box className="bg-[url('/pxbg2.png')] bg-cover h-[100vh] w-full">
