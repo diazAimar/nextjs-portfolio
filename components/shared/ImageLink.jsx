6b7293a1539ef8fc15f298b33afef52d2fa4191e
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-01-26 00:48:59 -0300

    ✨ Back to using videos.

--- components/shared/ImageLink.jsx
+++ components/shared/ImageLink.jsx
@@ -11,7 +11,7 @@ export default function ImageLink({ href, target, ext, sqr }) {
     <Box
       className={
         `shadow-[0_8px_0_0_rgba(14,7,27,1)] active:shadow-none active:translate-y-[8px] rounded-[15px] relative` +
-        (sqr ? ` w-[88px] sm:w-[110px] ` : ` w-[200px] sm:w-[250px]`)
+        (sqr ? ` w-[110px] ` : ` w-[250px]`)
       }
     >
       <ConditionalTooltipWrapper
@@ -27,17 +27,21 @@ export default function ImageLink({ href, target, ext, sqr }) {
         )}
       >
         <Link href={href} target={ext ? '_blank' : '_self'}>
-          <Box className="flex items-center justify-center">
-            <Box className="box z-20" />
-            <picture>
-              <img alt={target} src={sqr ? `/assets/${target}-button.png` : `/assets/button.png`} />
-            </picture>
-            {!sqr ? (
-              <Typography className="text-[1.5rem] sm:text-[2.5rem] capitalize absolute">
-                {target}
-              </Typography>
-            ) : null}
-          </Box>
+          <video
+            src={`/assets/${target}-button-hover.webm`}
+            onMouseOver={(e) => {
+              e.currentTarget.play();
+            }}
+            onMouseOut={(e) => {
+              e.currentTarget.pause();
+              e.currentTarget.currentTime = 0;
+            }}
+            muted
+            loop
+          >
+            Your browser does not support the <code>video</code> element. Click here to go to{' '}
+            {target}
+          </video>
         </Link>
       </ConditionalTooltipWrapper>
     </Box>
