252d916013a4f3acfd68db71a3e9f31901b54a98
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-02-02 00:51:02 -0300

    ✨ New fonts

--- styles/themes.js
+++ styles/themes.js
@@ -3,9 +3,40 @@ import { createTheme } from '@mui/material';
 export const theme1 = createTheme({
   typography: {
     allVariants: {
-      fontFamily: 'Abadon',
-      fontSize: '1.1rem',
-      letterSpacing: '0.15rem',
+      fontFamily: 'NicoPups',
+      fontSize: '1.5rem',
+      lineHeight: '1.2',
+      letterSpacing: '0.05rem',
+    },
+    h1: {
+      fontFamily: 'NicoPaint',
+      fontSize: '2.5rem',
+      color: '#f9e6cf',
+    },
+    h2: {
+      fontFamily: 'NicoPaint',
+      fontSize: '2rem',
+      color: '#f9e6cf',
+    },
+    h3: {
+      fontFamily: 'NicoPaint',
+      fontSize: '1.5rem',
+      color: '#f9e6cf',
+    },
+    h4: {
+      fontFamily: 'NicoPaint',
+      fontSize: '1.2rem',
+      color: '#f9e6cf',
+    },
+    h5: {
+      fontFamily: 'NicoPaint',
+      fontSize: '1rem',
+      color: '#f9e6cf',
+    },
+    h6: {
+      fontFamily: 'NicoPaint',
+      fontSize: '0.9rem',
+      color: '#f9e6cf',
     },
   },
 });
