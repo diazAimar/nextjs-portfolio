12fc6456c5b7f1a83bfbcb6fb9c77066574e451a
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-02-21 15:11:39 -0300

    ⚠ Fix warning

--- components/shared/ImageLink.jsx
+++ components/shared/ImageLink.jsx
@@ -47,7 +47,7 @@ export default function ImageLink({ href, target, ext, sqr }) {
         <Link href={href} target={ext ? '_blank' : '_self'} title={target}>
           <video
             src={`/assets/${targetLowercase}-button-hover.webm`}
-            disableRemotePlayback="true"
+            disableRemotePlayback={true}
             onMouseOver={(e) => {
               e.currentTarget.play();
             }}
